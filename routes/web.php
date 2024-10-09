a0f890945efd9a77e796674e9ec9a95e37ee029f
jesicangreni <iss22018@students.del.ac.id>
2024-10-09 16:32:56 +0700

    membuat halaman home

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/adminsite', function () {
     return view('adminsite');
 });
-Route::get('/', function () {
+
+Route::get('/login', function () {
     return view('Login');
+});
+
+Route::get('/home', function () {
+    return view('Home');
 });
