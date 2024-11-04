c483bebdfb2598a393a31dcef85c56c5dcb65836
jesicangreni <iss22018@students.del.ac.id>
2024-11-04 14:01:28 +0700

    memperbaiki halaman qna

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;  
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BusController;
 use App\Http\Controllers\QnAController;
 use App\Http\Controllers\Auth\LoginController;
@@ -22,25 +22,37 @@ use App\Http\Controllers\AuthController;
 // Rute untuk login
 Route::get('login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 // Rute untuk register
 Route::get('register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('register', [AuthController::class, 'register']);
-Route::post('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
-// Rute untuk Home
-Route::resource('/', HomeController::class);
+// Rute untuk halaman utama
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+// Rute halaman-halaman lain di HomeController
 Route::get('jadwal', [HomeController::class, 'Jadwal'])->name('jadwal');
 Route::get('rute', [HomeController::class, 'Rute'])->name('rute');
+Route::get('qna', [HomeController::class, 'QnA'])->name('qna');
 Route::get('kbt', [HomeController::class, 'KBT'])->name('kbt');
 Route::get('kpt', [HomeController::class, 'KPT'])->name('kpt');
 Route::get('tiomaz', [HomeController::class, 'TIOMAZ'])->name('tiomaz');
 Route::get('karyaagung', [HomeController::class, 'KaryaAgung'])->name('karyaagung');
-Route::get('home', [HomeController::class, 'index'])->name('home');
 
-// Rute untuk Bus
+// Rute tambahan untuk jadwal di BusController
 Route::get('jadwal', [BusController::class, 'index']);
 
-// Rute untuk QnA
-Route::get('qna', [QnAController::class, 'index'])->name('qna.index');
+// Rute untuk halaman QnA
+Route::get('qna', [QnAController::class, 'index'])->name('qna');
 Route::post('qna', [QnAController::class, 'store'])->name('qna.store');
+
+// Rute untuk halaman Dashboard
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
